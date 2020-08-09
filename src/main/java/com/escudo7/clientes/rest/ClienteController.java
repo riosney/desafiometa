957d730bc7200bf86c686df19ebc587beb9121f2
Riosney Santos <riosney_s@hotmail.com>
2020-08-08 21:11:44 -0300

    integracao com swagger

--- src/main/java/com/escudo7/clientes/rest/ClienteController.java
+++ src/main/java/com/escudo7/clientes/rest/ClienteController.java
@@ -2,6 +2,9 @@ package com.escudo7.clientes.rest;
 
 import com.escudo7.clientes.model.entity.Cliente;
 import com.escudo7.clientes.model.repository.ClienteRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/clientes")
 @CrossOrigin("http://localhost:4200")
+@Api(description = "Endpoints para criar, retornar, atualizar e deletar usuários.")
 public class ClienteController {
 
     private final ClienteRepository repository;
@@ -23,18 +27,22 @@ public class ClienteController {
     }
 
     @GetMapping
+    @ApiOperation("Retorna todos os usuários.")
     public List<Cliente> obterTodos(){
         return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva usuário.")
     public Cliente salvar(@RequestBody @Valid Cliente cliente ){
         return repository.save(cliente);
     }
 
     @GetMapping("{id}")
-    public Cliente acharPorId( @PathVariable Integer id ){
+    @ApiOperation("Retorna um usuário pelo seu ID.")
+    public Cliente acharPorId(@ApiParam("Id do usuário. Não pode ser vazio")
+                                  @PathVariable Integer id ){
         return repository
                 .findById(id)
                 .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado") );
@@ -42,7 +50,8 @@ public class ClienteController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletar( @PathVariable Integer id ){
+    @ApiOperation("Deleta um usuário pelo seu ID.")
+    public void deletar(@ApiParam("Id do usuário. Não pode ser vazio") @PathVariable Integer id ){
         repository
                 .findById(id)
                 .map( cliente -> {
@@ -54,7 +63,8 @@ public class ClienteController {
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar( @PathVariable Integer id,
+    @ApiOperation("Atualizar o usuário pelo seu ID.")
+    public void atualizar(@ApiParam("Id do usuário. Não pode ser vazio") @PathVariable Integer id,
                            @RequestBody @Valid Cliente clienteAtualizado ) {
         repository
                 .findById(id)
